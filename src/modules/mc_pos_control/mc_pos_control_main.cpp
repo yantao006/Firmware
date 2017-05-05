8442142de530ff995bfaa8cb28d9ad664eb3ae75
Lorenz Meier <lorenz@px4.io>
2017-05-05 15:01:27 +0200

    MC pos controller: log exceptions but rate-limit them

--- src/modules/mc_pos_control/mc_pos_control_main.cpp
+++ src/modules/mc_pos_control/mc_pos_control_main.cpp
@@ -263,6 +263,7 @@ private:
 	struct map_projection_reference_s _ref_pos;
 	float _ref_alt;
 	hrt_abstime _ref_timestamp;
+	hrt_abstime _last_warn;
 
 	math::Vector<3> _thrust_int;
 	math::Vector<3> _pos;
@@ -379,6 +380,8 @@ private:
 	 */
 	void limit_altitude();
 
+	void warn_rate_limited(const char *str);
+
 	/**
 	 * Shim for calling task_main from task_create.
 	 */
@@ -450,6 +453,7 @@ MulticopterPositionControl::MulticopterPositionControl() :
 	_user_intention_z(brake),
 	_ref_alt(0.0f),
 	_ref_timestamp(0),
+	_last_warn(0),
 	_yaw(0.0f),
 	_yaw_takeoff(0.0f),
 	_vel_z_lp(0),
@@ -561,6 +565,17 @@ MulticopterPositionControl::~MulticopterPositionControl()
 	}
 
 	pos_control::g_control = nullptr;
+}
+
+void
+MulticopterPositionControl::warn_rate_limited(const char *string)
+{
+	hrt_abstime now = hrt_absolute_time();
+
+	if (now - _last_warn > 50000) {
+		PX4_WARN(string);
+		_last_warn = now;
+	}
 }
 
 int
@@ -1534,7 +1549,7 @@ MulticopterPositionControl::control_offboard(float dt)
 					_vel_sp(1) = sinf(_yaw) * _pos_sp_triplet.current.vx + cosf(_yaw) * _pos_sp_triplet.current.vy;
 
 				} else {
-					PX4_WARN("Unknown velocity offboard coordinate frame");
+					warn_rate_limited("Unknown velocity offboard coordinate frame");
 				}
 
 				_run_pos_control = false;
@@ -1994,7 +2009,7 @@ void MulticopterPositionControl::control_auto(float dt)
 		if (!(PX4_ISFINITE(_pos_sp(0)) && PX4_ISFINITE(_pos_sp(1)) &&
 		      PX4_ISFINITE(_pos_sp(2)))) {
 
-			PX4_WARN("Auto: Position setpoint not finite");
+			warn_rate_limited("Auto: Position setpoint not finite");
 			_pos_sp = _curr_pos_sp;
 		}
 
@@ -2158,7 +2173,7 @@ MulticopterPositionControl::calculate_velocity_setpoint(float dt)
 		} else {
 			_vel_sp(0) = 0.0f;
 			_vel_sp(1) = 0.0f;
-			PX4_WARN("Caught invalid pos_sp in x and y");
+			warn_rate_limited("Caught invalid pos_sp in x and y");
 
 		}
 	}
@@ -2171,7 +2186,7 @@ MulticopterPositionControl::calculate_velocity_setpoint(float dt)
 
 		} else {
 			_vel_sp(2) = 0.0f;
-			PX4_WARN("Caught invalid pos_sp in z");
+			warn_rate_limited("Caught invalid pos_sp in z");
 		}
 
 	}
